0ba1552154e316106892bb61c8d3f306e2e06d59
silva-jr <josedasilva0112@gmail.com>
2022-08-15 14:24:22 +0100

    update agendamento

--- src/app/home/agendamento/agendamento.component.ts
+++ src/app/home/agendamento/agendamento.component.ts
@@ -1,31 +1,42 @@
 import { Component, OnInit } from '@angular/core';
 import { getDate, getDay } from 'date-fns';
 
-
 @Component({
   selector: 'app-agendamento',
   templateUrl: './agendamento.component.html',
-  styleUrls: ['./agendamento.component.scss']
+  styleUrls: ['./agendamento.component.scss'],
 })
 export class AgendamentoComponent implements OnInit {
-
   date: Date = new Date('2022-06-21');
-  days:any[]=[];
+  days: any[] = [];
   type: 'string';
-  modes = ['date', 'date-time', 'month', 'month-year', 'time', 'time-date', 'year'];
+  modes = [
+    'date',
+    'date-time',
+    'month',
+    'month-year',
+    'time',
+    'time-date',
+    'year',
+  ];
   selectedMode = 'date';
-  
+  myDate: String = new Date().toISOString();
 
-  constructor() { }
+  constructor() {}
 
   ngOnInit(): void {
-    this.days.push(getDate(new Date(this.date)))
-    console.log(getDate(new Date(this.date)));
+    const element = document.querySelectorAll('.time-hour');
+    element.forEach((current) => {
+      current.addEventListener('click', () => {
+        current.classList.toggle('clock-ative');        
+        
+        console.log(current.getAttribute('data-hours'))
+      });
+    });
+    console.log(this.myDate);
   }
 
-  onChange($event) {
-    console.log($event);
+  onChange(date) {
+    console.log(date);
   }
- 
-
 }
